9fd4cb03bfa8229d13a37eb72a47416d36c9fcb7
Viktor Varenik <yavarenikya@gmail.com>
2025-07-11 13:01:11 +0300

    feat: impl theme provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,7 @@
 import type {Metadata} from 'next';
 import './globals.css';
+import {ThemeProvider} from '@/components/theme-provider';
+import {Suspense} from 'react';
 
 export const metadata: Metadata = {
     title: 'Create Next App',
@@ -12,8 +14,19 @@ export default function RootLayout({
     children: React.ReactNode;
 }>) {
     return (
-        <html lang="en">
-            <body>{children}</body>
+        <html lang="en" suppressHydrationWarning>
+            <body>
+                <Suspense>
+                    <ThemeProvider
+                        attribute="class"
+                        defaultTheme="system"
+                        enableSystem
+                        disableTransitionOnChange
+                    >
+                        {children}
+                    </ThemeProvider>
+                </Suspense>
+            </body>
         </html>
     );
 }
